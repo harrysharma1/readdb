3b101b652f7eda83dcb65e7edd36313b1baa7a85
Harry Sharma <harrysharma1066@gmail.com>
2026-01-19 01:50:12 +0000

    feat: add static files to router and frontend tailwind/daisyui initialisation

--- Makefile
+++ Makefile
@@ -1,5 +1,11 @@
+.PHONY: css clean run
+
+css:
+	npx @tailwindcss/cli -i ./static/css/input.css -o ./static/css/output.css --minify
+
 clean:
 	@echo INITIALISING DB CLEAN
 	@test -f test.db && rm test.db || echo "NOTHING CLEANED"
-run: clean
+
+run: clean css
 	go run cmd/main.go
--- cmd/main.go
+++ cmd/main.go
@@ -18,6 +18,7 @@ func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Con
 	return t.templates.ExecuteTemplate(w, name, data)
 }
 func main() {
+	// DB
 	db, err := database.InitDB()
 	if err != nil {
 		log.Fatal(err)
@@ -26,11 +27,13 @@ func main() {
 	database.SeedAuthor(db)
 	database.SeedBook(db)
 	database.SeedList(db)
+	// Router
 	e := echo.New()
 	renderer := &Template{
 		templates: template.Must(template.ParseGlob("views/*.html")),
 	}
 	e.Renderer = renderer
+	e.Static("/static", "static")
 	handlers.RegisterHandlers(e, db)
 	e.Logger.Fatal(e.Start(":6969"))
 }
